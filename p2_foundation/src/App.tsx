ca13cfdb99aee1c12b80bf28c7023720d0d24002
krthompson89 <47638444+krthompson89@users.noreply.github.com>
2022-03-21 16:44:11 -0400

    Added create user screen

--- p2_foundation/src/App.tsx
+++ p2_foundation/src/App.tsx
@@ -5,11 +5,16 @@ import { Routes, Route } from 'react-router-dom';
 import Dashboard from './components/Dashboard';
 import Login from './components/Login';
 import { Principal } from './models/principal';
+import { CreateUser } from './components/CreateUser';
 
 
 //App component renders a login component and passes into  it authuser prop
 // and setcurrentuser prop
 function App() {
+
+  return (
+    <CreateUser/>
+   );
 
       //Generic- allows to take a Principal or undefined
   let [authUser, setAuthUser] = useState<Principal>();
