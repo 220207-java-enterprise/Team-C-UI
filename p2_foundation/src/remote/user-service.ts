f8fdf7e39b477d4ded2d164e2e74e684a71ae0d0
Andrew <Andrew.C.Saenz12@gmail.com>
2022-03-23 20:20:46 -0400

    working on register user

--- p2_foundation/src/remote/user-service.ts
+++ p2_foundation/src/remote/user-service.ts
@@ -1,6 +1,12 @@
-
+import { User } from "../models/user"
+import { appClient } from "./app-client"
 //register method
-export const register = () => {
+export const register = async (newUser : {username: string, email: string, password: string, firstname: string, lastname: string}) => {
+    return await appClient.post<User>('/user', newUser, {
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    });
     // TODO implement axios call to POST /users
 }
 
